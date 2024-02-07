dac5c63ecc0e29e8ab6b5a80e366acdc02a014fc
JABAN111 <368601@edu.itmo.ru>
2024-02-07 16:35:53 +0300

    Initial commit

---
+++ src/main/java/jaba/web/fourthWebLab/FourthWebLabApplication.java
@@ -0,0 +1,13 @@
+package jaba.web.fourthWebLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourthWebLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourthWebLabApplication.class, args);
+	}
+
+}
