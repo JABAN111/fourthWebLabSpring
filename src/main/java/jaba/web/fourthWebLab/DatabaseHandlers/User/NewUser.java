54979f7fdf589e85206f70941ef5282c0acc4ef5
JABAN111 <368601@edu.itmo.ru>
2024-02-10 18:51:36 +0300

    Служебная информация

---
+++ src/main/java/jaba/web/fourthWebLab/DatabaseHandlers/User/NewUser.java
@@ -0,0 +1,6 @@
+package jaba.web.fourthWebLab.DatabaseHandlers.User;
+
+public enum NewUser {
+    SUCCESSFULLY_CREATED,
+    USER_ALREADY_EXIST
+}
---
+++ src/main/java/jaba/web/fourthWebLab/DatabaseHandlers/User/statusUserInformation.java
@@ -0,0 +1,7 @@
+package jaba.web.fourthWebLab.DatabaseHandlers.User;
+
+public enum statusUserInformation {
+    USER_NOT_FOUND,
+    USER_VALID,
+    PASSWORD_INVALID
+}
