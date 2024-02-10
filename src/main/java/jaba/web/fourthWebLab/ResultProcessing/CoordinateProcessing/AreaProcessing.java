e700c6142a49ba359d63a99d88503ed88da45c9a
JABAN111 <368601@edu.itmo.ru>
2024-02-10 17:52:49 +0300

    Изменен тип R + пофикшена первая четверть(полуокружность)

--- src/main/java/jaba/web/fourthWebLab/ResultProcessing/CoordinateProcessing/AreaProcessing.java
+++ src/main/java/jaba/web/fourthWebLab/ResultProcessing/CoordinateProcessing/AreaProcessing.java
@@ -1,23 +1,26 @@
 package jaba.web.fourthWebLab.ResultProcessing.CoordinateProcessing;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AreaProcessing implements CartesianAreas {
 
     @Override
-    public boolean firstRotation(Double x, Double y, Float R) {
+    public boolean firstRotation(Double x, Double y, Double R) {
         if(x >= 0 && y >= 0){
             double circle = x*x+y*y;
-            return circle <= R*R && x>=R && y>=R;
+            return circle <= R*R && x<=R && y<=R;
         }
         return false;
     }
 
     @Override
-    public boolean secondRotation(Double x, Double y, Float R) {
+    public boolean secondRotation(Double x, Double y, Double R) {
         return false;
     }
 
     @Override
-    public boolean thirdRotation(Double x, Double y, Float R) {
+    public boolean thirdRotation(Double x, Double y, Double R) {
         if(x <= 0 && y <= 0){
             double line = -x/2 - R/2;
             return line<=y && x<=-R && y<=-R/2;
@@ -27,7 +30,7 @@ public class AreaProcessing implements CartesianAreas {
 
 
     @Override
-    public boolean fourthRotation(Double x, Double y, Float R) {
+    public boolean fourthRotation(Double x, Double y, Double R) {
         if(x>=0 && y <= 0){
             return x<=R && y>=-R/2;
         }
@@ -35,7 +38,7 @@ public class AreaProcessing implements CartesianAreas {
     }
 
     @Override
-    public boolean areaCheck(Double x, Double y, Float R) {
+    public boolean areaCheck(Double x, Double y, Double R) {
         return firstRotation(x,y,R) || secondRotation(x,y,R) || thirdRotation(x,y,R) || fourthRotation(x,y,R);
     }
 }
--- src/main/java/jaba/web/fourthWebLab/ResultProcessing/CoordinateProcessing/CartesianAreas.java
+++ src/main/java/jaba/web/fourthWebLab/ResultProcessing/CoordinateProcessing/CartesianAreas.java
@@ -1,9 +1,9 @@
 package jaba.web.fourthWebLab.ResultProcessing.CoordinateProcessing;
 
 public interface CartesianAreas {
-    boolean firstRotation(Double x, Double y, Float R);
-    boolean secondRotation(Double x, Double y, Float R);
-    boolean thirdRotation(Double x, Double y, Float R);
-    boolean fourthRotation(Double x,Double y,Float R);
-    boolean areaCheck(Double x,Double y,Float R);
+    boolean firstRotation(Double x, Double y, Double R);
+    boolean secondRotation(Double x, Double y, Double R);
+    boolean thirdRotation(Double x, Double y, Double R);
+    boolean fourthRotation(Double x,Double y,Double R);
+    boolean areaCheck(Double x, Double y, Double R);
 }
