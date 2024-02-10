bd6ba4d7b1d54677399768e319601c82be818964
JABAN111 <368601@edu.itmo.ru>
2024-02-10 14:28:40 +0300

    Классы для обработки результатов добавлены

---
+++ src/main/java/jaba/web/fourthWebLab/ResultProcessing/CoordinateProcessing/AreaProcessing.java
@@ -0,0 +1,41 @@
+package jaba.web.fourthWebLab.ResultProcessing.CoordinateProcessing;
+
+public class AreaProcessing implements CartesianAreas {
+
+    @Override
+    public boolean firstRotation(Double x, Double y, Float R) {
+        if(x >= 0 && y >= 0){
+            double circle = x*x+y*y;
+            return circle <= R*R && x>=R && y>=R;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean secondRotation(Double x, Double y, Float R) {
+        return false;
+    }
+
+    @Override
+    public boolean thirdRotation(Double x, Double y, Float R) {
+        if(x <= 0 && y <= 0){
+            double line = -x/2 - R/2;
+            return line<=y && x<=-R && y<=-R/2;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean fourthRotation(Double x, Double y, Float R) {
+        if(x>=0 && y <= 0){
+            return x<=R && y>=-R/2;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean areaCheck(Double x, Double y, Float R) {
+        return firstRotation(x,y,R) || secondRotation(x,y,R) || thirdRotation(x,y,R) || fourthRotation(x,y,R);
+    }
+}
